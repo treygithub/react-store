7cf26cf67f600915113d880a315a6afce1842a3f
Robert Hunnicutt <robhunn74@gmail.com>
2019-01-23 07:44:38 -0600

    add functionality to clear cart and delete an item

--- src/context.js
+++ src/context.js
@@ -8,7 +8,7 @@ class ProductProvider extends Component {
     state={
         products: [],
         detailProduct: detailProduct,
-        cart:storeProducts,
+        cart:[],
         modalOpen:false,
         modalProduct:detailProduct,
         cartSubTotal:0,
@@ -50,7 +50,7 @@ addToCart = (id) => {
         products:tempProducts,
         cart: [...this.state.cart,product]
       }
-    },()=>console.log('this.state', this.state))
+    },()=>{this.addTotals()});
 };
 
 openModal = id =>{
@@ -62,6 +62,7 @@ openModal = id =>{
     }
   })
 }
+
 closeModal = () =>{
   this.setState(()=>{
     return {modalOpen:false}
@@ -75,12 +76,53 @@ increment = (id) =>{
 decrement = (id) =>{
   console.log('decreated by one' );
 }
+
 deleteItem = (id) =>{
-  console.log('remove item' );
+  let tempProducts = [...this.state.products];
+  let tempCart = [...this.state.cart];
+
+  tempCart = tempCart.filter(item => item.id !== id)
+  let index = tempProducts.indexOf(this.getItem(id))
+  let removedProduct = tempProducts[index];
+  removedProduct.inCart = false;
+  removedProduct.count = 0;
+  removedProduct.total=0;
+
+  this.setState(()=>{
+    return {
+      cart:[...tempCart],
+      products:[...tempProducts]
+    }
+  },()=>{
+    this.addTotals();
+  })
 }
+
 clearCart = () =>{
-  console.log('cart cleared' )
+  this.setState(()=>{
+    return {cart:[]}
+  },()=>{
+    this.setProducts();
+    this.addTotals();
+  })
 }
+
+addTotals = ()=>{
+  let subTotal = 0;
+  this.state.cart.map(item=>(subTotal += item.total));
+  const tempTax = subTotal * .1;
+  const tax = parseFloat(tempTax.toFixed(2));
+  const total = subTotal +tax;
+  this.setState(()=>{
+    return{
+      cartSubTotal:subTotal,
+      cartTax:tax,
+      cartTotal:total
+    }
+  })
+
+}
+
   render() {
     return (
       <ProductContext.Provider value={{
